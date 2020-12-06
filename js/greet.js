c7609bf925d36ecf09b9151de091c3a11b72d025
Reza Ariftiarno <57562195+rezaariftiarno@users.noreply.github.com>
2020-12-06 13:12:29 +0700

    Update greet.js

--- js/greet.js
+++ js/greet.js
@@ -1,15 +1,15 @@
 var d = new Date();
 var time = d.getHours();
 
-if (time>0&&time<=11) {
+if (time>=0&&time<=11) {
   document.write("selamat pagi");
 }
-if (time>12&&time<=14) {
+if (time>=12&&time<=14) {
   document.write("selamat siang");
 }
-if (time>15&&time<=17) {
+if (time>=15&&time<=17) {
   document.write("selamat sore");
 }
-if (time>18&&time<=23) {
+if (time>=18&&time<=23) {
   document.write("selamat malam");
 }
