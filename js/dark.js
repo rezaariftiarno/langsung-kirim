40c371c61ba191fec5378e641d21433056a72f61
Reza Ariftiarno <57562195+rezaariftiarno@users.noreply.github.com>
2020-12-09 21:02:00 +0700

    Update dark.js

--- js/dark.js
+++ js/dark.js
@@ -2,9 +2,9 @@ function darkMode() {
    var element = document.body;
    var x = element.classList.toggle("dark-mode");
    if (x == true){
-   document.getElementById("dark").innerHTML = "Matikan Lampu";
+   document.getElementById("dark").innerHTML = "Nyalakan Lampu";
    }
    if (x == false){
-   document.getElementById("dark").innerHTML = "Nyalakan Lampu";
+   document.getElementById("dark").innerHTML = "Matikan Lampu";
    }
 }
